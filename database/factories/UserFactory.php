5c14922c5140b30fd334c6d033881c1202fa1a57
Rizki H.I <ibrahimriski211@gmail.com>
2026-06-08 19:36:52 +0900

    .

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -6,9 +6,10 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Faker\Factory as Faker;
 
 /**
- * @extends Factory<User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
@@ -22,16 +23,18 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
- public function definition(): array
-{
-    return [
-        'name' => $this->faker->name(),
-        'email' => $this->faker->unique()->safeEmail(),
-        'email_verified_at' => now(),
-        'password' => static::$password ??= Hash::make('password'),
-        'remember_token' => Str::random(10),
-    ];
-}
+    public function definition(): array
+    {
+        $faker = Faker::create();
+        
+        return [
+            'name' => $faker->name(),
+            'email' => $faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => static::$password ??= Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ];
+    }
 
     /**
      * Indicate that the model's email address should be unverified.
